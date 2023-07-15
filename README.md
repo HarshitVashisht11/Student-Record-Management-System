ab0146e3146bf81b1dd603b6c00b59f410de6773
Harshit Vashisht <harshitvashisht07@gmail.com>
2023-07-15 10:20:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Student Record Management System
+
+This project is a simple student record management system that allows you to store and manage student records. It provides functionalities to add new records, delete existing records, and search for records based on the city.
+The project is implemented in C++.
+
+## How it Works
+The program provides a menu-based interface where you can choose different options to perform various operations on the student records. Here's how it works:
+
+1. **Add a Record**: Allows you to add a new student record by entering the necessary information, including roll number, name, parent names, semester, branch, and address.
+
+2. **Delete a Record**: Lets you delete a student record by providing the roll number of the student.
+
+3. **Search by City**: Allows you to search for student records based on the city. Enter the city name, and the program will display the records of students belonging to that city.
+
+4. **Exit**: Terminates the program and exits.
+
+To use the program, follow the on-screen prompts and select the desired option from the menu. The program will perform the corresponding operation and provide feedback accordingly.
+
+Feel free to explore and modify the code according to your requirements!
+
---
+++ main.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+struct Student {
+    int rollNo;
+    string name;
+    string fatherName;
+    string motherName;
+    string className;
+    string semester;
+    string branch;
+    string houseNo;
+    string streetName;
+    string city;
+    string state;
+    string country;
+};
+
+void addRecord(Student records[], int& count) {
+    if (count == MAX_SIZE) {
+        cout << "Database is full. Cannot add more records.\n";
+        return;
+    }
+
+    Student newRecord;
+    cout << "Enter Roll No: ";
+    cin >> newRecord.rollNo;
+
+    for (int i = 0; i < count; i++) {
+        if (records[i].rollNo == newRecord.rollNo) {
+            cout << "Record with Roll No " << newRecord.rollNo << " already exists.\n";
+            return;
+        }
+    }
+
+    cout << "Enter Student Name: ";
+    cin.ignore();
+    getline(cin, newRecord.name);
+
+    cout << "Enter Father's Name: ";
+    getline(cin, newRecord.fatherName);
+
+    cout << "Enter Mother's Name: ";
+    getline(cin, newRecord.motherName);
+
+    cout << "Enter Class: ";
+    getline(cin, newRecord.className);
+
+    cout << "Enter Semester: ";
+    getline(cin, newRecord.semester);
+
+    cout << "Enter Branch: ";
+    getline(cin, newRecord.branch);
+
+    cout << "Enter Address:\n";
+    cout << "House No: ";
+    getline(cin, newRecord.houseNo);
+
+    cout << "Street Name: ";
+    getline(cin, newRecord.streetName);
+
+    cout << "City: ";
+    getline(cin, newRecord.city);
+
+    cout << "State: ";
+    getline(cin, newRecord.state);
+
+    cout << "Country: ";
+    getline(cin, newRecord.country);
+
+    records[count] = newRecord;
+    count++;
+    cout << "Record added successfully.\n";
+}
+
+void deleteRecord(Student records[], int& count, int rollNo) {
+    bool found = false;
+    for (int i = 0; i < count; i++) {
+        if (records[i].rollNo == rollNo) {
+            found = true;
+            for (int j = i; j < count - 1; j++) {
+                records[j] = records[j + 1];
+            }
+            count--;
+            cout << "Record with Roll No " << rollNo << " deleted.\n";
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Record with Roll No " << rollNo << " not found.\n";
+    }
+}
+
+void searchByCity(const Student records[], int count, const string& city) {
+    cout << "Records of students in " << city << ":\n";
+    bool found = false;
+    for (int i = 0; i < count; i++) {
+        if (records[i].city == city) {
+            found = true;
+            cout << "Roll No: " << records[i].rollNo << endl;
+            cout << "Name: " << records[i].name << endl;
+            cout << "Father's Name: " << records[i].fatherName << endl;
+            cout << "Mother's Name: " << records[i].motherName << endl;
+            cout << "Class: " << records[i].className << endl;
+            cout << "Semester: " << records[i].semester << endl;
+            cout << "Branch: " << records[i].branch << endl;
+            cout << "Address: " << records[i].houseNo << ", "
+                 << records[i].streetName << ", "
+                 << records[i].city << ", "
+                 << records[i].state << ", "
+                 << records[i].country << endl;
+            cout << endl;
+        }
+    }
+    if (!found) {
+        cout << "No records found in " << city << ".\n";
+    }
+}
+
+int main() {
+    Student records[MAX_SIZE];
+    int count = 0;
+
+    int choice = 0;
+    while (choice != 4) {
+        cout << "-----------------------------\n";
+        cout << "Menu:\n";
+        cout << "1. Add a record\n";
+        cout << "2. Delete a record\n";
+        cout << "3. Search by City\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                addRecord(records, count);
+                break;
+            }
+            case 2: {
+                int rollNo;
+                cout << "Enter Roll No to delete: ";
+                cin >> rollNo;
+                deleteRecord(records, count, rollNo);
+                break;
+            }
+            case 3: {
+                string city;
+                cout << "Enter City name to search: ";
+                cin.ignore();
+                getline(cin, city);
+                searchByCity(records, count, city);
+                break;
+            }
+            case 4: {
+                cout << "Exiting program.\n";
+                break;
+            }
+            default: {
+                cout << "Invalid choice. Please try again.\n";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
